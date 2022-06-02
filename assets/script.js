332f69cb2c23d8863c13fe7929ece2a930ebbd5d
josh.moran501 <josh.moran501@gmail.com>
2022-06-02 14:16:52 -0400

    all timeblocks generate properly, input boxes adjust properly based on time

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,36 @@
+var container = document.getElementById("container")
+var rightNow = moment().format("HH")
+var textfield = document.getElementsByName("textfield")
 
+// generate timeblocks
+for (let hour=9; hour<18; hour++) {   
+    var timeDisplayed = moment(hour, `H`)
+    var hourID = document.getElementById(`${timeDisplayed.format(`H`)}`)
+    container.innerHTML += `<div class="row">
+    <div class="col hour">${timeDisplayed.format(`h A`)}</div>
+    <input name="textfield" type="text" id="${timeDisplayed.format(`HH`)}" class="col-10"></input>
+    <button id="${timeDisplayed.format(`H`)}btn" class="col saveBtn">&#128190</button>
+    </div>`;
+    // checkTime()
+}
+
+// change color of input box based on time
+for (let i=0; i<9; i++) {
+    var value = textfield[i].getAttribute("id")
+    if(rightNow > value){
+        textfield[i].classList.add("past");
+    }
+    if(rightNow == value){
+        textfield[i].classList.add("present");
+    }
+    if(rightNow < value){
+        textfield[i].classList.add("future");
+    }
+
+}
+
+
+// save inputted values
+function saveEvent () {
+
+}
