e6ba28e0b5dcb0cfb534ba8be6ae530dc1f548d5
josh.moran501 <josh.moran501@gmail.com>
2022-06-03 09:39:04 -0400

    saves entered values into local storage and displays them after refresh

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,10 @@
 var container = document.getElementById("container")
 var rightNow = moment().format("HH")
 var textfield = document.getElementsByName("textfield")
+var saveBtn = document.getElementsByClassName("saveBtn")
+var today = document.getElementById("currentDay")
+
+today.innerText = `Today's date is ${moment().format("MMMM Do YYYY")}`
 
 // generate timeblocks
 for (let hour=9; hour<18; hour++) {   
@@ -9,9 +13,8 @@ for (let hour=9; hour<18; hour++) {
     container.innerHTML += `<div class="row">
     <div class="col hour">${timeDisplayed.format(`h A`)}</div>
     <input name="textfield" type="text" id="${timeDisplayed.format(`HH`)}" class="col-10"></input>
-    <button id="${timeDisplayed.format(`H`)}btn" class="col saveBtn">&#128190</button>
-    </div>`;
-    // checkTime()
+    <button id="${timeDisplayed.format(`HH`)}btn" class="saveBtn col">&#128190</button>
+    </div>`
 }
 
 // change color of input box based on time
@@ -26,11 +29,19 @@ for (let i=0; i<9; i++) {
     if(rightNow < value){
         textfield[i].classList.add("future");
     }
-
 }
 
+// save values to local storage, any x+9 values are to get it to match up with the hour in military time, which my ids use
+for(let x=0; x<9; x++) {
+    var btnValue = saveBtn[x].getAttribute("id")
+    this[btnValue].addEventListener("click",saveEvent)
+    function saveEvent () {
+    localStorage.setItem(x+9,textfield[x].value)
+    }
+    console.log(btnValue)
+}
 
-// save inputted values
-function saveEvent () {
-
+// add existing values to page after refresh
+for(let x=0; x<9; x++) {
+    textfield[x].value= localStorage.getItem(x+9)
 }
